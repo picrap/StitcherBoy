1ab90d9f810024ba14b31dedda330eafc7609a50
Pascal Craponne <pascal@craponne.fr>
2015-10-23 15:57:32 +0200

    Added project

---
+++ StitcherBoy/Stitcher.cs
@@ -0,0 +1,19 @@
+﻿namespace StitcherBoy
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using SticherBoy;
+
+    public class Stitcher: MarshalByRefObject
+    {
+        public void Run(string projectPath)
+        {
+            var project = new ProjectDefinition(projectPath);
+            var rawAssembly = File.ReadAllBytes(project.TargetPath);
+            var assembly = Assembly.ReflectionOnlyLoad(rawAssembly);
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assembly.GetName(), AssemblyBuilderAccess.RunAndSave);
+        }
+    }
+}
---
+++ StitcherBoy/StitcherBoyTask.cs
@@ -0,0 +1,50 @@
+﻿#region SignReferences
+// An automatic tool to presign unsigned dependencies
+// https://github.com/picrap/SignReferences
+#endregion
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using BuildTasker;
+using Microsoft.Build.Framework;
+using SticherBoy;
+using StitcherBoy;
+using StitcherBoy.Utility;
+
+/// <summary>
+/// Main entry point for module.
+/// </summary>
+// ReSharper disable once ClassNeverInstantiated.Global
+// ReSharper disable once CheckNamespace
+public class StitcherBoyTask : Tasker<StitcherBoyTask>
+{
+    /// <summary>
+    /// Gets or sets the project path (this is injected in the task).
+    /// </summary>
+    /// <value>
+    /// The project path.
+    /// </value>
+    [Required]
+    public string ProjectPath { get; set; }
+
+    /// <summary>
+    /// Gets or sets the solution path (also injected by task).
+    /// </summary>
+    /// <value>
+    /// The solution path.
+    /// </value>
+    public string SolutionPath { get; set; }
+
+    /// <summary>
+    /// Command-line entry point.
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public static void Main(string[] args) => Instance.Run(args);
+
+    public override void Run()
+    {
+        AppDomainUtility.InvokeSeparated<Stitcher>(s => s.Run(ProjectPath));
+    }
+}
