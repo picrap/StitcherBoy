aa3052b6727eb1e527267c6b5f494512c7eda57e
Pascal Craponne <pascal@craponne.fr>
2016-10-12 22:04:20 +0200

    Adding forgotten file
    Because something in the .gitignore exclude *assembly*

---
+++ StitcherBoy/Weaving/Build/AssemblyDependency.cs
@@ -0,0 +1,63 @@
+﻿#region Arx One
+// Stitcher Boy - a small library to help building post-build taks
+// https://github.com/picrap/StitcherBoy
+// MIT License - http://opensource.org/licenses/MIT
+#endregion
+
+namespace StitcherBoy.Weaving.Build
+{
+    using System.Diagnostics;
+    using dnlib.DotNet;
+
+    /// <summary>
+    /// Represents a dependency
+    /// </summary>
+    [DebuggerDisplay("{Path}, private={IsPrivate}")]
+    public class AssemblyDependency
+    {
+        /// <summary>
+        /// Gets a value indicating whether this instance is private.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is private; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsPrivate { get; }
+
+        /// <summary>
+        /// Gets the path.
+        /// </summary>
+        /// <value>
+        /// The path.
+        /// </value>
+        public string Path { get; }
+
+        private ModuleDef _module;
+
+        /// <summary>
+        /// Gets the module.
+        /// </summary>
+        /// <value>
+        /// The module.
+        /// </value>
+        public ModuleDef Module
+        {
+            get
+            {
+                if (_module == null)
+                    _module = ModuleDefMD.Load(Path);
+                return _module;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyDependency"/> class.
+        /// </summary>
+        /// <param name="isPrivate">if set to <c>true</c> [is private].</param>
+        /// <param name="path">The path.</param>
+        public AssemblyDependency(string path, bool isPrivate)
+        {
+            IsPrivate = isPrivate;
+            Path = path;
+        }
+    }
+}
