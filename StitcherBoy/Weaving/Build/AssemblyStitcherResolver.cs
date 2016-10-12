44efba66d6609412e51185422f9efd8f8afd4120
Pascal Craponne <pascal@craponne.fr>
2016-10-12 22:06:34 +0200

    Adding forgotten file AGAIN

---
+++ StitcherBoy/Weaving/Build/AssemblyStitcherResolver.cs
@@ -0,0 +1,28 @@
+﻿#region Arx One
+// Stitcher Boy - a small library to help building post-build taks
+// https://github.com/picrap/StitcherBoy
+// MIT License - http://opensource.org/licenses/MIT
+#endregion
+
+namespace StitcherBoy.Weaving.Build
+{
+    using System.Collections.Generic;
+    using dnlib.DotNet;
+ public   class AssemblyStitcherResolver: AssemblyResolver
+    {
+        protected override IEnumerable<string> GetModuleSearchPaths(ModuleDef module)
+        {
+            return base.GetModuleSearchPaths(module);
+        }
+
+        protected override IEnumerable<string> FindAssemblies(IAssembly assembly, ModuleDef sourceModule, bool matchExactly)
+        {
+            return base.FindAssemblies(assembly, sourceModule, matchExactly);
+        }
+
+        protected override IEnumerable<string> PostFindAssemblies(IAssembly assembly, ModuleDef sourceModule, bool matchExactly)
+        {
+            return base.PostFindAssemblies(assembly, sourceModule, matchExactly);
+        }
+    }
+}
